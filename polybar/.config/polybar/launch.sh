853d55183e25aaa61b2c7c08d288af7b1f328d2a
Nadhif Ginola <nfginola@gmail.com>
2026-05-19 07:56:47 +0200

    Polybar: Conditional battery

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,6 +2,12 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.1; done
 
+if ls /sys/class/power_supply/BAT* &>/dev/null; then
+    export POLYBAR_MODULES_RIGHT="volume battery"
+else
+    export POLYBAR_MODULES_RIGHT="volume"
+fi
+
 # Match lines with "connected" and a resolution (e.g. 2560x1440+0+0), skipping
 # outputs that are connected but inactive (no resolution in the line).
 xrandr | awk '/connected/ && /[0-9]+x[0-9]+\+/ {print $1}' | while read -r monitor; do
